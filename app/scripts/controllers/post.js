ec6581d98e3e196d6fe229766657843e17747ab9
Darin Minamoto <darinm223@gmail.com>
2015-04-04 16:49:34 -0700

    Might not work

--- app/scripts/controllers/post.js
+++ app/scripts/controllers/post.js
@@ -8,23 +8,39 @@ angular.module('auditionApp')
 	var ref = new Firebase("https://auditionr.firebaseio.com/users/" + $rootScope.authId + "/productions")
 	var pArray = $firebaseArray(ref)
 
-	$scope.characters = [""]
+	$scope.characters = [""];
+    $scope.productionname = "";
+    $scope.companyname = "";
+    $scope.samplescript = "";
+    $scope.description = "";
+
+    $scope.error = null;
 
 	$scope.postClick = function() {
-		$scope.posts = pArray
+        if ($scope.characters.trim() === '' ||
+            $scope.productionname.trim() === '' ||
+            $scope.companyname.trim() === '' ||
+            $scope.samplescript.trim() === '' ||
+            $scope.description.trim() === '') {
 
-        console.log($scope.characters);
-        var trimmedArray = $scope.characters.filter(function(elem) { return elem.trim() !== '' });
-        console.log(trimmedArray);
+          
+        } else {
+		  $scope.posts = pArray
 
-		$scope.posts.$add({
-			productionName: $scope.productionname,
-			companyName: $scope.companyname,
-			characters: trimmedArray, 
-			sampleScript: $scope.samplescript,
-			descriptionBox: $scope.description
-        });
-        $location.path('/browse');
+          console.log($scope.characters);
+          var trimmedArray = $scope.characters.filter(function(elem) { return elem.trim() !== '' });
+          console.log(trimmedArray);
+
+		  $scope.posts.$add({
+		  	productionName: $scope.productionname,
+		  	companyName: $scope.companyname,
+		  	characters: trimmedArray, 
+		  	sampleScript: $scope.samplescript,
+		  	descriptionBox: $scope.description
+          });
+          $location.path('/browse');
+        }
+
 	}
 
 	$scope.plusClick = function() {
