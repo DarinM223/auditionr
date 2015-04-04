fc9f6ffb67c2d5653d959b17c8580a74474c533c
Kay Lab <kaylab@air-14.local>
2015-04-04 14:04:14 -0700

    Implementing the request form
    Implement the request form

--- app/scripts/controllers/post.js
+++ app/scripts/controllers/post.js
@@ -2,6 +2,15 @@
 
 angular.module('auditionApp')
 
-.controller('PostCtrl', function ($scope) {
+.controller('PostCtrl', function ($scope, $firebaseArray) {
+	var ref = new Firebase("https://auditionr.firebaseio.com/posts")
+	console.log("HELLO WORLD!!!!")
+	var pArray = $firebaseArray(ref)
+
+	$scope.postClick = function() {
+		$scope.posts = pArray
+
+		// console.log("This is " + pArray)
+	}
 
 });
