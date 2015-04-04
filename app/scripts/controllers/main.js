7590b983dc0ca5f0249cd9550f76c0864fe1e535
Nicholas Egan <nicholas@egansoft.com>
2015-04-04 10:13:21 -0700

    Added firebase and facebook auth

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,37 @@
  * Controller of the auditionApp
  */
 angular.module('auditionApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+
+.controller('MainCtrl', function ($scope, $location) {
+
+  function authDataCallback(authData) {
+    console.log('authDataCallback')
+    if(authData) {
+      console.log("User " + authData.uid + " is logged in with " + authData.provider);
+      console.log('redirecting')
+      $location.path('/browse')
+      $location.replace()
+    }
+  }
+
+  $scope.fbLogin = function() {
+    ref.authWithOAuthPopup("facebook", function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        console.log("Authenticated successfully with payload:", authData);
+        authDataCallback(ref.getAuth())
+      }
+    });
+  }
+
+  $scope.checkLogin = function() {
+    var authData = ref.getAuth();
+    authDataCallback(authData)
+  }
+
+  var ref = new Firebase("https://auditionr.firebaseio.com");
+  ref.onAuth(authDataCallback);
+
+});
