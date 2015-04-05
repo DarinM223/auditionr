ac2d4740e32fddf796d16f95abe61fc97eb971ae
Nicholas Egan <nicholas@egansoft.com>
2015-04-04 17:22:59 -0700

    idk

--- app/scripts/controllers/browse.js
+++ app/scripts/controllers/browse.js
@@ -2,7 +2,7 @@
 
 angular.module('auditionApp')
 
-.controller('BrowseCtrl', function ($scope, $firebaseArray) {
+.controller('BrowseCtrl', function ($scope, $firebaseArray, $rootScope) {
   var pRef = new Firebase("https://auditionr.firebaseio.com/users")
   var pArray = $firebaseArray(pRef)
 
@@ -29,14 +29,53 @@ angular.module('auditionApp')
 
   $scope.submit = function() {
     var production = $scope.current
+    var charId = $('input:radio[name=character]:checked').val();
 
     console.log(production)
 
-    // var aRef = new Firebase("https://auditionr.firebaseio.com/users/" + production.ref().parent().name() +
-    //   "/productions/" + $scope.id + "/auditions");
+    var aRef = new Firebase("https://auditionr.firebaseio.com/users/" + production.user +
+      "/productions/" + $scope.id + "/auditions");
 
-    // pArray.$add({
-    //
-    // })
+    var aArray = $firebaseArray(aRef)
+
+    aArray.$loaded().then(function(auditions) {
+
+      var auditions = production.auditions
+
+      function addAudition() {
+        auditions.$add({people: [{id: $rootScope.authId, charId: charId}]})
+      }
+
+      if(auditions.length === 0) {
+        // You're the first one to audition for this play
+        addAudition()
+      } else {
+        // There's others who signed up already
+        var found = false
+
+        for(var i=0;i<auditions.length;i++) {
+          var audition = auditions[i]
+          var has = false
+
+          for(var j=0;j<audition.people.length;j++) {
+            if(audition.people[j].charId === charId)
+              has = true
+          }
+
+          if(!has) {
+            // There is a spot in this audition available for you
+            audition.people.$add({id: $rootScope.authId, charId: charId})
+            break;
+          }
+        }
+
+        if(!found) {
+          // There's no audition with a spot available for you
+          addAudition()
+        }
+
+      }
+
+    })
   }
 });
