efd0dc71824432e66f877bdd4e5b26ff76da6b25
Darin Minamoto <darinm223@gmail.com>
2015-04-04 18:24:05 -0700

    Fixed my_casting page

--- app/scripts/controllers/my_casting.js
+++ app/scripts/controllers/my_casting.js
@@ -2,27 +2,24 @@
 
 angular.module('auditionApp')
 
-.controller('MyCastingCtrl', function ($scope, $rootScope) {
+.controller('MyCastingCtrl', function ($scope, $rootScope, $firebaseObject) {
 	if($rootScope.authId != null)
 		console.log($rootScope.authId)
-	var pRef = new Firebase("https://auditionr.firebaseio.com/users")
-  	
-	
-  // 	var pArray = $firebaseArray(pRef)
+	var pRef = new Firebase("https://auditionr.firebaseio.com/users/" + $rootScope.authId + "/productions")
+    var pArray = $firebaseObject(pRef);
 
+    $scope.error = null;
+    $scope.productions = {};
 
-
- 	// pArray.$loaded().then(function(arr) {
-  //   $scope.productions = {};
-
-  //   for (var i = 0; i < pArray.length; i++) {
-  //     $scope.productions = _.extend($scope.productions, _.map(pArray[i].productions,function(production) {
-  //       console.log(pArray[i]);
-  //       return _.extend(production, { user: pArray[i].$id });
-  //     }));
-  //   }
-
-  //   console.log($scope.productions);
-
-  // })
+    pArray.$loaded().then(function(arr) {
+      var newObj = {};
+      _.each(pArray, function(value, key, obj) {
+        if (typeof(key) !== 'undefined') {
+          var production = value;
+          newObj[key] = _.extend(production, { user: $rootScope.authId });
+        }
+      });
+      $scope.productions = _.extend($scope.productions, newObj);
+      console.log($scope.productions);
+    });
 });
